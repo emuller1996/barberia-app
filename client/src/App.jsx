1c308b680b0d6451d701d22a0d854e7706fe7294
emuller1996 <stefano.muller@hotmail.com>
2024-05-31 12:26:44 -0500

    ADD BACK

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages";
 import MainTemplate from "./templates/_MainTemplate";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "http://18.230.187.240:3000/";
 function App() {
   return (
     <>
