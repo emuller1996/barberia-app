c9c0ce12fed3ce1b621745e7be391867c3167f43
emuller1996 <stefano.muller@hotmail.com>
2024-07-23 18:35:36 -0500

    REMOVE CONFIG VITE

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  /* publicDir: "./public/", */
-  base: "./",
-  /* assetsInclude: "./public/assets", */
+  plugins: [react()]
 });
