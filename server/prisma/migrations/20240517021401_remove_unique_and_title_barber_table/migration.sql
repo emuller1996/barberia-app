95b6f9b65f1ba7fb907466a57c224288f8e71013
emuller1996 <stefano.muller@hotmail.com>
2024-07-22 20:35:29 -0500

    REMOVE PRISMA

--- server/prisma/migrations/20240517021401_remove_unique_and_title_barber_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Barber_email_key` ON `barber`;
-
--- AlterTable
-ALTER TABLE `barber` ADD COLUMN `title` VARCHAR(191) NULL;
--- server/prisma/migrations/20240522001103_fix_relations/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `appointment` DROP FOREIGN KEY `Appointment_clientId_fkey`;
-
--- AlterTable
-ALTER TABLE `appointment` MODIFY `clientId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Appointment` ADD CONSTRAINT `Appointment_clientId_fkey` FOREIGN KEY (`clientId`) REFERENCES `Client`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
